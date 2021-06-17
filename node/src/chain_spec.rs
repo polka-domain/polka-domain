602059d7cf2da1e851371aac7a71d5c7a1b3cf1a
Alkaid <alkaidwe@gmail.com>
2021-06-17 22:00:40 +0800

    new chain spec (#37)

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -16,12 +16,11 @@
 // limitations under the License.
 
 use cumulus_primitives_core::ParaId;
-use hex_literal::hex;
 use parachain_runtime::{AccountId, AuraId, Signature};
 use sc_chain_spec::{ChainSpecExtension, ChainSpecGroup};
 use sc_service::ChainType;
 use serde::{Deserialize, Serialize};
-use sp_core::{crypto::UncheckedInto, sr25519, Pair, Public};
+use sp_core::{sr25519, Pair, Public};
 use sp_runtime::traits::{IdentifyAccount, Verify};
 
 /// Specialized `ChainSpec` for the normal parachain runtime.
@@ -61,15 +60,73 @@ where
 	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
 }
 
-pub fn get_chain_spec(id: ParaId) -> ChainSpec {
+pub fn development_config(id: ParaId) -> ChainSpec {
+	ChainSpec::from_genesis(
+		"Polka Domain Development",
+		"polka_domain_dev",
+		ChainType::Development,
+		move || {
+			genesis(
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				vec![
+					get_from_seed::<AuraId>("Alice"),
+				],
+				vec![
+					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+				],
+				id,
+			)
+		},
+		vec![],
+		None,
+		None,
+		None,
+		Extensions { relay_chain: "rococo-dev".into(), para_id: id.into() },
+	)
+}
+
+pub fn local_testnet_config(id: ParaId) -> ChainSpec {
 	ChainSpec::from_genesis(
 		"Polka Domain Local Testnet",
 		"polka_domain_local_testnet",
 		ChainType::Local,
 		move || {
-			testnet_genesis(
+			genesis(
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
-				vec![get_from_seed::<AuraId>("Alice"), get_from_seed::<AuraId>("Bob")],
+				vec![
+					get_from_seed::<AuraId>("Alice"),
+					get_from_seed::<AuraId>("Bob"),
+				],
+				vec![
+					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+				],
+				id,
+			)
+		},
+		vec![],
+		None,
+		None,
+		None,
+		Extensions { relay_chain: "rococo-local".into(), para_id: id.into() },
+	)
+}
+
+pub fn genesis_config(id: ParaId) -> ChainSpec {
+	ChainSpec::from_genesis(
+		"Polka Domain",
+		"polka_domain_mainnet",
+		ChainType::Local,
+		move || {
+			genesis(
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				vec![
+					get_from_seed::<AuraId>("Alice"),
+					get_from_seed::<AuraId>("Bob"),
+				],
 				vec![
 					get_account_id_from_seed::<sr25519::Public>("Alice"),
 					get_account_id_from_seed::<sr25519::Public>("Bob"),
@@ -91,41 +148,11 @@ pub fn get_chain_spec(id: ParaId) -> ChainSpec {
 		None,
 		None,
 		None,
-		Extensions { relay_chain: "westend-dev".into(), para_id: id.into() },
+		Extensions { relay_chain: "rococo".into(), para_id: id.into() },
 	)
 }
 
-pub fn staging_test_net(id: ParaId) -> ChainSpec {
-	ChainSpec::from_genesis(
-		"Polka Domain Staging Testnet",
-		"polka_domain_staging_testnet",
-		ChainType::Live,
-		move || {
-			testnet_genesis(
-				hex!["9ed7705e3c7da027ba0583a22a3212042f7e715d3c168ba14f1424e2bc111d00"].into(),
-				vec![
-					// $secret//one
-					hex!["aad9fa2249f87a210a0f93400b7f90e47b810c6d65caa0ca3f5af982904c2a33"]
-						.unchecked_into(),
-					// $secret//two
-					hex!["d47753f0cca9dd8da00c70e82ec4fc5501a69c49a5952a643d18802837c88212"]
-						.unchecked_into(),
-				],
-				vec![
-					hex!["9ed7705e3c7da027ba0583a22a3212042f7e715d3c168ba14f1424e2bc111d00"].into()
-				],
-				id,
-			)
-		},
-		Vec::new(),
-		None,
-		None,
-		None,
-		Extensions { relay_chain: "westend-dev".into(), para_id: id.into() },
-	)
-}
-
-fn testnet_genesis(
+fn genesis(
 	root_key: AccountId,
 	initial_authorities: Vec<AuraId>,
 	endowed_accounts: Vec<AccountId>,
